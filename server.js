b41647c1e9b8d0bf568b19b9ec10aac46c7ec97d
ChildDonation <judnerzephir1@gmail.com>
2025-05-30 22:29:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,28 @@ app.post('/create-checkout-session', async (req, res) => {
       quantity: 1,
     }],
     mode: 'payment',
+    success_url: `${req.protocol}://${req.get('host')}/success.html`,
+    cancel_url: `${req.protocol}://${req.get('host')}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+app.post('/create-checkout-session', async (req, res) => {
+  const amount = parseInt(req.body.amount, 10); // sa li soti nan form lan
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'Donation',
+        },
+        unit_amount: amount,
+      },
+      quantity: 1,
+    }],
+    mode: 'payment',
     success_url: ${req.protocol}://${req.get('host')}/success.html,
     cancel_url: ${req.protocol}://${req.get('host')}/cancel.html,
   });
